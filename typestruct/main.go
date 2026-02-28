81e115adaa1e22ed84c00f8d2176deb6f5d86ab4
Liberbell <liberbell@gmail.com>
2026-02-28 12:23:17 +0900

    main4

--- typestruct/main.go
+++ typestruct/main.go
@@ -7,8 +7,8 @@ var s = "seven"
 func main() {
 	var s2 = "six"
 
-	log.Println(s)
-	log.Println(s2)
+	log.Println("s is:", s)
+	log.Println("s2 is:", s2)
 }
 
 func saySomething(s string) (string, string) {
