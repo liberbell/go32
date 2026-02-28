3d1c6b61017bef4523c298f12dc71b175876b22c
Liberbell <liberbell@gmail.com>
2026-02-28 12:26:15 +0900

    main5

--- typestruct/main.go
+++ typestruct/main.go
@@ -9,8 +9,11 @@ func main() {
 
 	log.Println("s is:", s)
 	log.Println("s2 is:", s2)
+
+	saySomething("xxx")
 }
 
 func saySomething(s string) (string, string) {
+	log.Println("s from saySomething func is:", s)
 	return s, "world"
 }
