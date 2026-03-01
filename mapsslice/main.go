85aeff527b21077f260a2a07aa0f1d19a2da2608
Liberbell <liberbell@gmail.com>
2026-03-01 16:09:39 +0900

    main1

--- mapsslice/main.go
+++ mapsslice/main.go
@@ -1,6 +1,15 @@
 package main
 
+import "log"
+
 func main() {
 	var myString string
 	var myInt int
+
+	myString = "Hi"
+	myInt = 11
+
+	mySecondString := "another string"
+
+	log.Println(myString, mySecondString, myInt)
 }
