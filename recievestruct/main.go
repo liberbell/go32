44e250e414c868b5af08e0b5b0e95976bcd879e0
Liberbell <liberbell@gmail.com>
2026-03-01 16:02:44 +0900

    main2

--- recievestruct/main.go
+++ recievestruct/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 type myStruct struct {
 	FirstName string
 }
@@ -11,4 +13,7 @@ func main() {
 	myVar2 := myStruct{
 		FirstName: "Elton",
 	}
+	log.Println("myVar is set to:", myVar)
+	log.Println("myVar2 is set to:", myVar2)
+
 }
