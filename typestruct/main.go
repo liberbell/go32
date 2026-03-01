e094a8343b3e07ad7fa26ccf9f1fdc02f86f62ba
Liberbell <liberbell@gmail.com>
2026-03-01 15:42:28 +0900

    main3

--- typestruct/main.go
+++ typestruct/main.go
@@ -12,6 +12,14 @@ var phoneNumber string
 var age int
 var birthDate time.Time
 
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
+
 func main() {
 	var s2 = "six"
 
