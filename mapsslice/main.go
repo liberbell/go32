a138b4941c452ea03207de1f84a8ce620b7f7b04
Liberbell <liberbell@gmail.com>
2026-03-03 08:13:42 +0900

    main3

--- mapsslice/main.go
+++ mapsslice/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "log"
-
 // type User struct {
 // 	FirstName string
 // 	LastName  string
@@ -47,9 +45,11 @@ func main() {
 	// sort.Ints(mySlice)
 
 	// log.Println(mySlice)
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	log.Println(numbers)
-	log.Println(numbers[0:2])
-	log.Println(numbers[6:9])
+	// log.Println(numbers)
+	// log.Println(numbers[0:2])
+	// log.Println(numbers[6:9])
+
+	names := []string{"one", "seven", "fish", "cat"}
 }
