e472f8e9fd8ed0e4823fc281d72902623e1d9597
Liberbell <liberbell@gmail.com>
2026-03-04 08:33:46 +0900

    main7

--- structures/main.go
+++ structures/main.go
@@ -18,17 +18,25 @@ func main() {
 	// } else {
 	// 	log.Println("cat is not cat")
 	// }
-	myNum := 100
-	isTrue := true
+	// myNum := 100
+	// isTrue := true
 
-	if myNum > 99 && !isTrue {
-		log.Println("My num is greater than 99 and isTrue is set to false")
-	} else if myNum < 100 && isTrue {
-		log.Println("My num is less than 100 and isTrue is set to true")
-	} else if myNum == 101 || isTrue {
-		log.Println("Mynum is set to 101 or isTrue is set to true")
-	} else if myNum > 1000 && isTrue == false {
-		log.Println("Mynum is greater than 1000 and isTrue is set to false")
+	// if myNum > 99 && !isTrue {
+	// 	log.Println("My num is greater than 99 and isTrue is set to false")
+	// } else if myNum < 100 && isTrue {
+	// 	log.Println("My num is less than 100 and isTrue is set to true")
+	// } else if myNum == 101 || isTrue {
+	// 	log.Println("Mynum is set to 101 or isTrue is set to true")
+	// } else if myNum > 1000 && isTrue == false {
+	// 	log.Println("Mynum is greater than 1000 and isTrue is set to false")
+	// }
+
+	myVar := "cat"
+
+	switch myVar {
+	case "cat":
+		log.Println("myVar is set to cat")
+
 	}
 
 }
