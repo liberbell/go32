44367fba6078a1b9d4b73cad4625088dfac63be3
Liberbell <liberbell@gmail.com>
2026-03-04 08:14:51 +0900

    main2

--- structures/main.go
+++ structures/main.go
@@ -21,8 +21,10 @@ func main() {
 	myNum := 100
 	isTrue := false
 
-	if myNum > 99 && isTrue == false {
+	if myNum > 99 && !isTrue {
 		log.Println("My num is greater than 99 and isTrue is set to True")
+	} else if myNum < 100 && isTrue {
+		log.Println(("My num is less than 100 and isTrue is set to true"))
 	}
 
 }
