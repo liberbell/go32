b8f535ce6a50ca200340af0024852ea1ac9c7c93
Liberbell <liberbell@gmail.com>
2026-03-04 09:03:26 +0900

    main3

--- loops/main.go
+++ loops/main.go
@@ -3,7 +3,12 @@ package main
 import "log"
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		log.Println("Loop count is: ", i)
+	// for i := 0; i <= 10; i++ {
+	// 	log.Println("Loop count is: ", i)
+	// }
+
+	animals := []string{"dog", "fish", "horse", "cow", "cat"}
+	for i, animal := range animals {
+		log.Println()
 	}
 }
