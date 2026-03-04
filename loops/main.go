bdce61ba974aa6356c3e57d60a467fbb6587692b
Liberbell <liberbell@gmail.com>
2026-03-05 08:35:36 +0900

    main10

--- loops/main.go
+++ loops/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "log"
-
 func main() {
 	// for i := 0; i <= 10; i++ {
 	// 	log.Println("Loop count is: ", i)
@@ -11,10 +9,24 @@ func main() {
 	// animals := make(map[string]string)
 	// animals["dog"] = "fido"
 	// animals["cat"] = "fluffy"
-	var firstLine = "Once upon a midnight breary"
-	firstLine = "x"
+	// var firstLine = "Once upon a midnight breary"
+	// firstLine = "x"
 
-	for i, l := range firstLine {
-		log.Println(i, l)
+	// for i, l := range firstLine {
+	// 	log.Println(i, ":", l)
+	// }
+
+	type User struct {
+		FirstLName string
+		LastName   string
+		Email      string
+		Age        int
+	}
+
+	var users []User
+	users = append(users, User{"Elton", "John", "elton@example.com", 78})
+
+	for i, l := range ; i, l < count; i, l++ {
+		
 	}
 }
