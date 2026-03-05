35a020fdfdf20f26c94e7acacfdbd20546b84127
Liberbell <liberbell@gmail.com>
2026-03-06 08:56:44 +0900

    main5

--- interfaces/main.go
+++ interfaces/main.go
@@ -23,8 +23,12 @@ func main() {
 		Name:  "Eric",
 		Breed: "Coagie",
 	}
+	PrintInfo(dog)
 }
 
 func PrintInfo(a Animal) {
 	fmt.Println("This animal says ", a.Says(), "and has ", a.NumberOfLegs(), " legs")
+}
+func (d Dog) Says() string {
+	return "woof"
 }
