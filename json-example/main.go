908971a1d7b5587d49667fdab2673c2be138ebf4
Liberbell <liberbell@gmail.com>
2026-03-08 18:18:00 +0900

    main6

--- json-example/main.go
+++ json-example/main.go
@@ -5,19 +5,19 @@ import (
 	"log"
 )
 
-// type Person struct {
-// 	FirstName string `json: "first_name"`
-// 	LastName  string `json: "last_name"`
-// 	HairColor string `json: "hair_color"`
-// 	HasDog    bool   `json: "has_dog"`
-// }
-
 type Person struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	HairColor string `json:"hair_color"`
 	HasDog    bool   `json:"has_dog"`
 }
+
+// type Person struct {
+// 	FirstName string `json:"first_name"`
+// 	LastName  string `json:"last_name"`
+// 	HairColor string `json:"hair_color"`
+// 	HasDog    bool   `json:"has_dog"`
+// }
 
 func main() {
 
