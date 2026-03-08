360ed8242f639aff98528afb9bcbd4b742156e82
Liberbell <liberbell@gmail.com>
2026-03-09 08:28:14 +0900

    main_test2

--- testing_course/main_test.go
+++ testing_course/main_test.go
@@ -8,3 +8,10 @@ func TestDivide(t *testing.T) {
 		t.Error("Got an error when we shoud not have")
 	}
 }
+
+func TestBadDivide(t *testing.T) {
+	_, err := divide(10.0, 0)
+	if err == nil {
+		t.Error("Did not got an error when we shoud not have")
+	}
+}
