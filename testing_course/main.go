a7f4a8f39d4e9b1d2054483ce5480acd89efc127
Liberbell <liberbell@gmail.com>
2026-03-09 08:23:18 +0900

    main3

--- testing_course/main.go
+++ testing_course/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"log"
+)
 
 func main() {
-
+	result, err := divide(100.0, 10.0)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("result of division is ", result)
 }
 
 func divide(x, y float32) (float32, error) {
