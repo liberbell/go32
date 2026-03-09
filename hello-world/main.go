d49a7207541674a0ff77befbe152055442428754
Liberbell <liberbell@gmail.com>
2026-03-10 08:36:07 +0900

    main4

--- hello-world/main.go
+++ hello-world/main.go
@@ -9,6 +9,9 @@ func main() {
 	// fmt.Println("Hello-world")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		n, err := fmt.Fprintf(w, "Hello World")
-		fmt.Println(fmt.Sprintf("Number of bytes written: " + n))
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
 	})
 }
