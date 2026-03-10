197105825fb296d93f39719da4dc5f50f93a034e
Liberbell <liberbell@gmail.com>
2026-03-10 12:13:05 +0900

    main7

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,15 +5,22 @@ import (
 	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "This is the home page.")
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	// 	n, err := fmt.Fprintf(w, "Hello World")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	// })
+}
+
 func main() {
 	// fmt.Println("Hello-world")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	http.HandleFunc("/", home)
 
 	_ = http.ListenAndServe(":8080", nil)
 }
