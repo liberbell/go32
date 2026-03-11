db1358002a79e40969e7f1c5975d83face23caee
Liberbell <liberbell@gmail.com>
2026-03-12 08:32:56 +0900

    main5

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,56 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-// func Divide(w http.ResponseWriter, r *http.Request) {
-// 	var x float32
-// 	var y float32
-// 	x = 100.0
-// 	y = 0.0
-// 	f, err := DivideValuse(x, y)
-// 	if err != nil {
-// 		fmt.Fprintf(w, "Cannot divide by zero")
-// 		return
-// 	}
-// 	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %.02f ", x, y, f))
-// }
-
-// func AddValuse(x, y int) int {
-// 	var sum int
-// 	sum = x + y
-// 	return sum
-// }
-
-// func DivideValuse(x, y float32) (float32, error) {
-// 	var result float32
-// 	if y <= 0 {
-// 		err := errors.New("cannot divide by zero")
-// 		return 0, err
-// 	}
-// 	result = x / y
-// 	return result, nil
-// }
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parseTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template: ", err)
-		return
-	}
-}
 
 func main() {
 	// fmt.Println("Hello-world")
