078c5a27474ae4fa2413487b74da0fb704044477
Liberbell <liberbell@gmail.com>
2026-03-12 08:13:34 +0900

    main1

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
@@ -10,56 +9,46 @@ const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "This is the home page.")
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-	// 	n, err := fmt.Fprintf(w, "Hello World")
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	// })
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := AddValuse(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+
 }
 
-func Divide(w http.ResponseWriter, r *http.Request) {
-	var x float32
-	var y float32
-	x = 100.0
-	y = 0.0
-	f, err := DivideValuse(x, y)
-	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by zero")
-		return
-	}
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %.02f ", x, y, f))
-}
+// func Divide(w http.ResponseWriter, r *http.Request) {
+// 	var x float32
+// 	var y float32
+// 	x = 100.0
+// 	y = 0.0
+// 	f, err := DivideValuse(x, y)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Cannot divide by zero")
+// 		return
+// 	}
+// 	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %.02f ", x, y, f))
+// }
 
-func AddValuse(x, y int) int {
-	var sum int
-	sum = x + y
-	return sum
-}
+// func AddValuse(x, y int) int {
+// 	var sum int
+// 	sum = x + y
+// 	return sum
+// }
 
-func DivideValuse(x, y float32) (float32, error) {
-	var result float32
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result = x / y
-	return result, nil
-}
+// func DivideValuse(x, y float32) (float32, error) {
+// 	var result float32
+// 	if y <= 0 {
+// 		err := errors.New("cannot divide by zero")
+// 		return 0, err
+// 	}
+// 	result = x / y
+// 	return result, nil
+// }
 
 func main() {
 	// fmt.Println("Hello-world")
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
+	// http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting Web server on port %s", portNumber))
 
