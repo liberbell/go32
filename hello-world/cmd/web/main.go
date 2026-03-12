cdc457545b1f3d3ac99e1fc8485d83d0d5f416c0
Liberbell <liberbell@gmail.com>
2026-03-13 08:35:06 +0900

    main2

--- hello-world/cmd/web/main.go
+++ hello-world/cmd/web/main.go
@@ -3,14 +3,16 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/liber/myniceprogram/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 func main() {
 	// fmt.Println("Hello-world")
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 	// http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting Web server on port %s", portNumber))
