cff0cf4c243af60d6d552e5e366ed192e23ffb7f
Liberbell <liberbell@gmail.com>
2026-03-14 17:06:24 +0900

    render2

--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -7,8 +7,9 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parseTemplate.Execute(w, nil)
+	// parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template: ", err)
 		return
