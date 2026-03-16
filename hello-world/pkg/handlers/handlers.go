35783ef66af1b3b52479008e2704d87826648f0e
Liberbell <liberbell@gmail.com>
2026-03-17 08:37:22 +0900

    handlers2

--- hello-world/pkg/handlers/handlers.go
+++ hello-world/pkg/handlers/handlers.go
@@ -13,6 +13,16 @@ type Repository struct {
 	App *config.AppConfig
 }
 
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
