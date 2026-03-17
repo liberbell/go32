8537a002df8b9cc44792b7211947cb872d2f5fd0
Liberbell <liberbell@gmail.com>
2026-03-18 08:40:59 +0900

    handlers2

--- hello-world/pkg/handlers/handlers.go
+++ hello-world/pkg/handlers/handlers.go
@@ -11,6 +11,11 @@ type TemplateData struct {
 	StringMap map[string]string
 	IntMap    map[string]int
 	FloatMap  map[string]float32
+	Data      map[string]interface{}
+	CSRFToken string
+	Flash     string
+	Warning   string
+	Error     string
 }
 
 var Repo *Repository
