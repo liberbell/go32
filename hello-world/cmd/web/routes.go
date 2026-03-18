aebed6427ddc6a47544ce7a19c5641519cc83bbd
Liberbell <liberbell@gmail.com>
2026-03-19 08:42:18 +0900

    routes2

--- hello-world/cmd/web/routes.go
+++ hello-world/cmd/web/routes.go
@@ -3,9 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat"
 	"github.com/liber/myniceprogram/pkg/config"
+	"github.com/liber/myniceprogram/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
+	mux := pat.New()
 
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 }
