f894128debd382c9e050d17decc870e8b7ab72f9
Liberbell <liberbell@gmail.com>
2026-03-20 14:07:43 +0900

    middleware2

--- hello-world/cmd/web/middleware.go
+++ hello-world/cmd/web/middleware.go
@@ -8,5 +8,6 @@ import (
 func WriteToConsole(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Hit the page")
+		next.ServeHTTP(w, r)
 	})
 }
