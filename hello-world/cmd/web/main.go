eb53e60b1b622d525a409a1fa82f3b5cb43d4815
Liberbell <liberbell@gmail.com>
2026-03-22 07:19:36 +0900

    main3

--- hello-world/cmd/web/main.go
+++ hello-world/cmd/web/main.go
@@ -17,11 +17,13 @@ const portNumber = ":8080"
 func main() {
 	var app config.AppConfig
 
+	app.InProduction = false
+
 	session := scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
 	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = false
+	session.Cookie.Secure = app.InProduction
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
