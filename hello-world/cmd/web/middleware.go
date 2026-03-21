167f881ed31a0af39dc927e9ee49af7cb1a5da00
Liberbell <liberbell@gmail.com>
2026-03-21 21:03:49 +0900

    middleware3

--- hello-world/cmd/web/middleware.go
+++ hello-world/cmd/web/middleware.go
@@ -16,4 +16,9 @@ func WriteToConsole(next http.Handler) http.Handler {
 
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+	})
 }
