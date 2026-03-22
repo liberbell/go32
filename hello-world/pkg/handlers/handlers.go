c769e56ca5695f399138525b4bff00b9628eb8fc
Liberbell <liberbell@gmail.com>
2026-03-23 08:21:31 +0900

    handlers1

--- hello-world/pkg/handlers/handlers.go
+++ hello-world/pkg/handlers/handlers.go
@@ -25,6 +25,9 @@ func NewHandlers(r *Repository) {
 }
 
 func (m Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_IP", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
