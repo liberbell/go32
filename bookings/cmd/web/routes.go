eca34599bd23c5e7198fea8d31183e7e92a67707
Liberbell <liberbell@gmail.com>
2026-03-24 08:28:08 +0900

    routes1

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/liber/myniceprogram/pkg/config"
-	"github.com/liber/myniceprogram/pkg/handlers"
+	"github.com/liber/bookings/pkg/config"
+	"github.com/liber/bookings/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
