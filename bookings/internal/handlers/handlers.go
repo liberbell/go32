7097d0a7cc415e8be1d0b9e4f81d922680e3114e
Liberbell <liberbell@gmail.com>
2026-04-18 08:06:44 +0900

    handlers4

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -65,6 +65,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
+
 }
 
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
