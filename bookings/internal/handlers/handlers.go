36eb1a05c1874c3b4555aae229003a6a45400869
Liberbell <liberbell@gmail.com>
2026-04-21 08:50:39 +0900

    handlers3

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -81,6 +81,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	form := forms.New(r.PostForm)
 	form.Required("first_name", "last_name", "email", "phone")
 	form.MinLength("first_name", 3, r)
+	from.IsEmail("email")
 
 	// form.Has("first_name", r)
 	if !form.Valid() {
