aaf873736d3e5f7be4119e2f7084324d94ed2f0e
Liberbell <liberbell@gmail.com>
2026-04-22 12:21:01 +0900

    routes2

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -25,10 +25,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
-	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
+	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 
 	fileserver := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
