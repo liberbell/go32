c2d3ed4c3a022b1e87df6d931f9d91df005f2cbe
Liberbell <liberbell@gmail.com>
2026-04-28 08:57:27 +0900

    setup_test1

--- bookings/internal/handlers/setup_test.go
+++ bookings/internal/handlers/setup_test.go
@@ -1,1 +1,45 @@
 package handlers
+
+import (
+	"encoding/gob"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/liber/bookings/internal/config"
+	"github.com/liber/bookings/internal/handlers"
+	"github.com/liber/bookings/internal/models"
+	"github.com/liber/bookings/internal/render"
+)
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func getRoutes(t *testing.T) http.Handler {
+	gob.Register(models.Reservation{})
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+		return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+}
