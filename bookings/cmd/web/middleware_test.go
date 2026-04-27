f39c2e82f709838cd94fad8083bbf72521c565ac
Liberbell <liberbell@gmail.com>
2026-04-28 08:35:34 +0900

    middleware_test2

--- bookings/cmd/web/middleware_test.go
+++ bookings/cmd/web/middleware_test.go
@@ -16,3 +16,15 @@ func TestNoSurf(t *testing.T) {
 		t.Errorf("type is not http.handler, but is %T", v)
 	}
 }
+
+func TestNoSessionLoad(t *testing.T) {
+	var myH myHandler
+
+	h := SessionLoad(&myH)
+
+	switch v := h.(type) {
+	case http.Handler:
+	default:
+		t.Errorf("type is not http.handler, but is %T", v)
+	}
+}
