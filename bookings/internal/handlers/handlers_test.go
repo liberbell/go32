5372beeff5980afb3a3aa0439f89b953b66cda96
Liberbell <liberbell@gmail.com>
2026-04-30 09:01:38 +0900

    handlers_test3

--- bookings/internal/handlers/handlers_test.go
+++ bookings/internal/handlers/handlers_test.go
@@ -56,19 +56,20 @@ func TestHandlers(t *testing.T) {
 			}
 			if resp.StatusCode != e.expectedStatusCode {
 				t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
-			} else {
-				values := url.Values{}
-				for _, x := range e.params {
-					values.Add(x.key, x.value)
-				}
-				resp, err := ts.Client().PostForm(ts.URL+e.url, values)
-				if err != nil {
-					t.Log(err)
-					t.Fatal(err)
-				}
-				if resp.StatusCode != e.expectedStatusCode {
-					t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
-				}
+			}
+
+		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
 		}
 	}
