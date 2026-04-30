5f4c8b7adc4a335aabdbbd1ba2b5903dfeaececb
Liberbell <liberbell@gmail.com>
2026-04-30 09:08:18 +0900

    setup_test1

---
+++ bookings/internal/render/setup_test.go
@@ -0,0 +1,15 @@
+package render
+
+import (
+	"testing"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/liber/bookings/internal/config"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+}
