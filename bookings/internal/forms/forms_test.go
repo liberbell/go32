ade76a6cebcda09c11d60b9b3b97a4968072a3f3
Liberbell <liberbell@gmail.com>
2026-05-03 21:12:43 +0900

    forms_test2

--- bookings/internal/forms/forms_test.go
+++ bookings/internal/forms/forms_test.go
@@ -7,4 +7,10 @@ import (
 
 func Testform_Valid(t *testing.T) {
 	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
 }
