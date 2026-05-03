f7ebf1c4d81f66483ecf5e5eb525b58160601a7c
Liberbell <liberbell@gmail.com>
2026-05-03 21:39:56 +0900

    forms2

--- bookings/internal/forms/forms.go
+++ bookings/internal/forms/forms.go
@@ -44,7 +44,7 @@ func (f *Form) Has(field string, r *http.Request) bool {
 }
 
 func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+	x := f.Get(field)
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long.", length))
 		return false
