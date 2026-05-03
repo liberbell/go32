3ab86bdda518c0ed9a9b52744959d40ff89013f7
Liberbell <liberbell@gmail.com>
2026-05-03 21:37:28 +0900

    forms1

--- bookings/internal/forms/forms.go
+++ bookings/internal/forms/forms.go
@@ -35,7 +35,7 @@ func (f *Form) Required(fields ...string) {
 }
 
 func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
+	x := f.Get(field)
 	if x == "" {
 		f.Errors.Add(field, "This field cannot be blank")
 		return false
