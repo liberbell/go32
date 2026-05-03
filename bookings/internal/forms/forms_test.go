2fe865cb863c91be4f9e7278a679b43b60baf3f5
Liberbell <liberbell@gmail.com>
2026-05-03 21:15:52 +0900

    forms_test3

--- bookings/internal/forms/forms_test.go
+++ bookings/internal/forms/forms_test.go
@@ -14,3 +14,13 @@ func Testform_Valid(t *testing.T) {
 		t.Error("got invalid when should have been valid")
 	}
 }
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	if form.Valid() {
+		t.Error("form shows valid when required fields missing")
+	}
+}
