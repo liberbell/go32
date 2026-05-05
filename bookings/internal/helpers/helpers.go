2928e6a49b871b8040d84d4c3f86b902151033d3
eliberbell <liberbell@gmail.com>
2026-05-05 13:03:18 +0900

    helpers2

--- bookings/internal/helpers/helpers.go
+++ bookings/internal/helpers/helpers.go
@@ -1,9 +1,21 @@
 package helpers
 
-import "github.com/liber/bookings/internal/config"
+import (
+	"net/http"
+
+	"github.com/liber/bookings/internal/config"
+)
 
 var app *config.AppConfig
 
 func NewHelpers(a *config.AppConfig) {
 	app = a
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+	http.Error(w)
+}
+
+func ServerError(w http.ResponseWriter, err error) {
+
 }
