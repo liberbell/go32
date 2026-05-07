7898e047c4bd8eada5bc0e0a01c537c4353d2504
eliberbell <liberbell@gmail.com>
2026-05-08 08:37:37 +0900

    schema1

--- bookings/migrations/schema.sql
+++ bookings/migrations/schema.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 4TI0QVnwF51xcl6HfmNVUjcIwdwkClJIcL6Y5ansCJXpvFfCkrp7ViCKkyXrCCG
+\restrict xZIqwMG3WGSEhTRopxyikoLikDuxDNxUlRCo8HfE5qnLsDe0c6JaGobTsQs54rK
 
 -- Dumped from database version 18.3
 -- Dumped by pg_dump version 18.3
@@ -24,6 +24,48 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: reservations; Type: TABLE; Schema: public; Owner: bookings_ope
+--
+
+CREATE TABLE public.reservations (
+    id integer NOT NULL,
+    first_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    last_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    email character varying(255) NOT NULL,
+    phone character varying(255) NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    room_id integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.reservations OWNER TO bookings_ope;
+
+--
+-- Name: reservations_id_seq; Type: SEQUENCE; Schema: public; Owner: bookings_ope
+--
+
+CREATE SEQUENCE public.reservations_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.reservations_id_seq OWNER TO bookings_ope;
+
+--
+-- Name: reservations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: bookings_ope
+--
+
+ALTER SEQUENCE public.reservations_id_seq OWNED BY public.reservations.id;
+
+
+--
 -- Name: schema_migration; Type: TABLE; Schema: public; Owner: bookings_ope
 --
 
@@ -35,11 +77,81 @@ CREATE TABLE public.schema_migration (
 ALTER TABLE public.schema_migration OWNER TO bookings_ope;
 
 --
+-- Name: users; Type: TABLE; Schema: public; Owner: bookings_ope
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    first_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    last_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    email character varying(255) NOT NULL,
+    password character varying(60) NOT NULL,
+    access_level integer DEFAULT 1 NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO bookings_ope;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: bookings_ope
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_id_seq OWNER TO bookings_ope;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: bookings_ope
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: reservations id; Type: DEFAULT; Schema: public; Owner: bookings_ope
+--
+
+ALTER TABLE ONLY public.reservations ALTER COLUMN id SET DEFAULT nextval('public.reservations_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: bookings_ope
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: reservations reservations_pkey; Type: CONSTRAINT; Schema: public; Owner: bookings_ope
+--
+
+ALTER TABLE ONLY public.reservations
+    ADD CONSTRAINT reservations_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: schema_migration schema_migration_pkey; Type: CONSTRAINT; Schema: public; Owner: bookings_ope
 --
 
 ALTER TABLE ONLY public.schema_migration
     ADD CONSTRAINT schema_migration_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: bookings_ope
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
 --
@@ -53,5 +165,5 @@ CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USIN
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 4TI0QVnwF51xcl6HfmNVUjcIwdwkClJIcL6Y5ansCJXpvFfCkrp7ViCKkyXrCCG
+\unrestrict xZIqwMG3WGSEhTRopxyikoLikDuxDNxUlRCo8HfE5qnLsDe0c6JaGobTsQs54rK
 
