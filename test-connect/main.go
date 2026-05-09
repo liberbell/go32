a3c7c4b70894690b76ef7fd5701fb882cf66ca87
eliberbell <liberbell@gmail.com>
2026-05-09 14:53:12 +0900

    main2

--- test-connect/main.go
+++ test-connect/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "database/sql"
 
+func main() {
+	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=booking_ope password=pass1234")
 }
