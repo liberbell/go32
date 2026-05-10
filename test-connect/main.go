9a8cf3ef65f5d687b40bebff90f88f1e5aae182f
eliberbell <liberbell@gmail.com>
2026-05-10 11:04:06 +0900

    main3

--- test-connect/main.go
+++ test-connect/main.go
@@ -8,8 +8,16 @@ import (
 )
 
 func main() {
-	_, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=booking_ope password=pass1234")
+	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=booking_ope password=pass1234")
 	if err != nil {
 		log.Fatalf("Anable to connect: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Connect to database.")
+
+	err = conn.Ping()
+	if err != nil {
+		log.Fatal("cannot ping database:", err)
 	}
 }
