3f726fcc761ec486117d8d6369e07dba174558f4
eliberbell <liberbell@gmail.com>
2026-05-10 10:52:32 +0900

    main1

--- test-connect/main.go
+++ test-connect/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 func main() {
-	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=booking_ope password=pass1234")
+	_, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=booking_ope password=pass1234")
+	if err != nil {
+		log.Fatalf("Anable to connect: %v", err)
+	}
 }
