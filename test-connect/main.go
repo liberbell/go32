b4c451f98cbdb3f94c70095fd5ce0146f1b7c0a3
eliberbell <liberbell@gmail.com>
2026-05-11 09:13:22 +0900

    main7

--- test-connect/main.go
+++ test-connect/main.go
@@ -42,6 +42,17 @@ func main() {
 	err = getAllRows(conn)
 	if err != nil {
 		log.Fatal(err)
+
+		query = `select id, first_name, last_name from users where id = $1`
+
+		var firstName, lastName string
+		var id int
+
+		rows := conn.QueryRow(query, 1)
+		err = rows.Scan(&id, &firstName, &lastName)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	stmt := `update users set first_name = $1 where first_name = $2`
