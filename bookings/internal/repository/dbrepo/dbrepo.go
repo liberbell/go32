d6fa910eb8af3e85a1d77ccf34f597a083fb4bfa
eliberbell <liberbell@gmail.com>
2026-05-13 08:24:53 +0900

    dbrepo1

--- bookings/internal/repository/dbrepo/dbrepo.go
+++ bookings/internal/repository/dbrepo/dbrepo.go
@@ -4,9 +4,17 @@ import (
 	"database/sql"
 
 	"github.com/liber/bookings/internal/config"
+	"github.com/liber/bookings/internal/repository"
 )
 
 type PostgresDBRepo struct {
 	App *config.AppConfig
 	DB  *sql.DB
+}
+
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &PostgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
 }
