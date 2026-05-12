2a25cb0d03ebf938909678b612e197eddd593f39
eliberbell <liberbell@gmail.com>
2026-05-13 08:59:49 +0900

    models1

--- bookings/internal/models/models.go
+++ bookings/internal/models/models.go
@@ -1,8 +1,21 @@
 package models
 
+import "time"
+
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
