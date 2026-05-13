3156c8eb39f1241f6bb4af49c66b812fa058a697
eliberbell <liberbell@gmail.com>
2026-05-14 08:21:09 +0900

    setup_test1

--- bookings/cmd/web/main.go
+++ bookings/cmd/web/main.go
@@ -84,7 +84,7 @@ func run() (*driver.DB, error) {
 
 	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
-	render.NewTemplates(&app)
+	render.NewRender(&app)
 	helpers.NewHelpers(&app)
 
 	return db, nil
