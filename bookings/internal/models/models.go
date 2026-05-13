640cd5174383b36f263d057aa5ba69f01b944442
eliberbell <liberbell@gmail.com>
2026-05-14 08:05:06 +0900

    models4

--- bookings/internal/models/models.go
+++ bookings/internal/models/models.go
@@ -2,14 +2,14 @@ package models
 
 import "time"
 
-type Reservation struct {
-	FirstName string
-	LastName  string
-	Email     string
-	Phone     string
-}
+// type Reservation struct {
+// 	FirstName string
+// 	LastName  string
+// 	Email     string
+// 	Phone     string
+// }
 
-type Users struct {
+type User struct {
 	ID          int
 	FirstName   string
 	LastName    string
@@ -20,21 +20,21 @@ type Users struct {
 	UpdatedAt   time.Time
 }
 
-type Rooms struct {
+type Room struct {
 	ID        int
 	RoomName  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-type Restrictions struct {
+type Restriction struct {
 	ID              int
 	RestrictionName string
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
 }
 
-type Reservations struct {
+type Reservation struct {
 	ID        int
 	FirstName string
 	LastName  string
@@ -45,10 +45,10 @@ type Reservations struct {
 	RoomID    int
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	Room      Rooms
+	Room      Room
 }
 
-type RoomRestrictions struct {
+type RoomRestriction struct {
 	ID            int
 	StartDate     time.Time
 	EndDate       time.Time
@@ -57,7 +57,7 @@ type RoomRestrictions struct {
 	RestrictionID int
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
-	Room          Rooms
-	Reservation   Reservations
-	Restriction   Restrictions
+	Room          Room
+	Reservation   Reservation
+	Restriction   Restriction
 }
