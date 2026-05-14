884c5c98f1b533c6710e4e5b66bed950676dcc6c
eliberbell <liberbell@gmail.com>
2026-05-15 08:33:27 +0900

    postgres2

--- bookings/internal/repository/dbrepo/postgres.go
+++ bookings/internal/repository/dbrepo/postgres.go
@@ -7,5 +7,12 @@ func (m *PostgresDBRepo) AllUsers() bool {
 }
 
 func (m *PostgresDBRepo) InsertReservation(res models.Reservation) error {
+
+	stmt := `insert into reservations
+			(first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at) 
+		values
+			($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+		returning id`
+
 	return nil
 }
