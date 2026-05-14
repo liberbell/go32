6d5e18d03d8b1c45240d4a974aaaaa7d5d47263e
eliberbell <liberbell@gmail.com>
2026-05-15 08:43:38 +0900

    postgres5

--- bookings/internal/repository/dbrepo/postgres.go
+++ bookings/internal/repository/dbrepo/postgres.go
@@ -1,6 +1,7 @@
 package dbrepo
 
 import (
+	"context"
 	"time"
 
 	"github.com/liber/bookings/internal/models"
@@ -11,13 +12,15 @@ func (m *PostgresDBRepo) AllUsers() bool {
 }
 
 func (m *PostgresDBRepo) InsertReservation(res models.Reservation) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
 	stmt := `insert into reservations
 			(first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at) 
 		values
 			($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 
-	_, err := m.DB.Exec(stmt,
+	_, err := m.DB.ExecContext(ctx, stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
