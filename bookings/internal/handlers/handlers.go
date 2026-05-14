f226964e95a1ae5701f79ca4ea8a3b6073f0d94b
eliberbell <liberbell@gmail.com>
2026-05-15 08:51:41 +0900

    handlers1

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/liber/bookings/internal/config"
 	"github.com/liber/bookings/internal/driver"
@@ -63,11 +64,25 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	layout := "2006-01-02"
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: r.Form.Get(startDate),
 	}
 
 	form := forms.New(r.PostForm)
