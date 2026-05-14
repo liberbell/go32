c0c31d78929417e857ca0d0bddb3cd899ba6c0ed
eliberbell <liberbell@gmail.com>
2026-05-15 08:30:09 +0900

    postgres1

--- bookings/internal/repository/dbrepo/postgres.go
+++ bookings/internal/repository/dbrepo/postgres.go
@@ -1,5 +1,11 @@
 package dbrepo
 
+import "github.com/liber/bookings/internal/models"
+
 func (m *PostgresDBRepo) AllUsers() bool {
 	return true
+}
+
+func (m *PostgresDBRepo) InsertReservation(res models.Reservation) error {
+	return nil
 }
