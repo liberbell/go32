a9bc6c3dcf442c299b866bda25c05c2ea838dd3d
eliberbell <liberbell@gmail.com>
2026-05-16 19:45:12 +0900

    postgres2

--- bookings/internal/repository/dbrepo/postgres.go
+++ bookings/internal/repository/dbrepo/postgres.go
@@ -11,16 +11,18 @@ func (m *PostgresDBRepo) AllUsers() bool {
 	return true
 }
 
-func (m *PostgresDBRepo) InsertReservation(res models.Reservation) error {
+func (m *PostgresDBRepo) InsertReservation(res models.Reservation) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	var newID int
 
 	stmt := `insert into reservations
 			(first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at) 
 		values
 			($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 
-	_, err := m.DB.ExecContext(ctx, stmt,
+	err := m.DB.QueryRowContext(ctx, stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
@@ -30,10 +32,11 @@ func (m *PostgresDBRepo) InsertReservation(res models.Reservation) error {
 		res.RoomID,
 		time.Now(),
 		time.Now(),
-	)
+	).Scan(&newID)
+
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	return nil
+	return newID, nil
 }
