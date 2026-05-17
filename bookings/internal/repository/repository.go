eec5f51b7f97059e70041da4314a221c04826343
eliberbell <liberbell@gmail.com>
2026-05-18 08:34:48 +0900

    repository1

--- bookings/internal/repository/repository.go
+++ bookings/internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/liber/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/liber/bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
