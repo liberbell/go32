ce752e070033432db410398e9995c2b3fd02f47a
eliberbell <liberbell@gmail.com>
2026-05-22 08:35:18 +0900

    handlers4

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -244,4 +244,6 @@ func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
 	}
 	res.RoomID = roomID
 	m.App.Session.Put(r.Context(), "reservation", res)
+
+	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
 }
