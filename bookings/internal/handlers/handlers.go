983e0ce34ef4bd98e9d5468068709a2c61130d6e
eliberbell <liberbell@gmail.com>
2026-05-22 08:31:44 +0900

    handlers2

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -238,4 +238,8 @@ func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Get(r.Context(), "reservation")
 
 	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
 }
