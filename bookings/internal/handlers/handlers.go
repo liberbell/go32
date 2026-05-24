2956f8f1db89a4cbafd8466cfe8e9b1d4a1f689c
eliberbell <liberbell@gmail.com>
2026-05-25 08:44:26 +0900

    handlers4

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -219,6 +219,10 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	roomID, _ := strconv.Atoi(r.Form.Get("room_id"))
 
 	available, _ := m.DB.SearchAvailabilityByDatesByRoomID(startDate, endDate, roomID)
+	resp := jsonResponse{
+		OK:      available,
+		Message: "",
+	}
 
 	out, err := json.MarshalIndent(resp, "", "     ")
 	if err != nil {
