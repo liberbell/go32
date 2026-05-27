d8d2ccbaef4ba82fa016d259d9f62dcfb5e41ca0
eliberbell <liberbell@gmail.com>
2026-05-28 08:02:37 +0900

    seetrestrictions1

---
+++ bookings/migrations/20260527230124_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2026-05-16 00:00:00.000','2026-05-16 00:00:00.000'),
+	 ('Owner block','2026-05-17 00:00:00.000','2026-05-17 00:00:00.000');
