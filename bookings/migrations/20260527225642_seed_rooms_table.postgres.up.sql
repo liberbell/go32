3bc82cbdc7d845e69f1309d4046329c4c6d6b4f0
eliberbell <liberbell@gmail.com>
2026-05-28 08:00:22 +0900

    seedrooms1

---
+++ bookings/migrations/20260527225642_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s quarters','2026-05-16 00:00:00.000','2026-05-16 00:00:00.000'),
+	 ('Major''s Suite','2026-05-16 00:00:00.000','2026-05-16 00:00:00.000');
