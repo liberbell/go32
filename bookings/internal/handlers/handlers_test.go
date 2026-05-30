4db13aee20cdd706da5e27464a2a1461253ba06c
eliberbell <liberbell@gmail.com>
2026-05-30 11:31:56 +0900

    handlers_test3

--- bookings/internal/handlers/handlers_test.go
+++ bookings/internal/handlers/handlers_test.go
@@ -95,21 +95,21 @@ func TestRepository_Reservation(t *testing.T) {
 	rr := httptest.NewRecorder()
 	session.Put(ctx, "reservation", reservation)
 
-	handlers := http.HandlerFunc(Repo.Reservation)
-	handlers.ServeHTTP(rr, req)
+	handler := http.HandlerFunc(Repo.Reservation)
+	handler.ServeHTTP(rr, req)
 
 	if rr.Code != http.StatusOK {
-		t.Errorf("Reservation hundler returned wrong response code: got %d, wanted %d", rr.Code, http.StatusOK)
+		t.Errorf("Reservation handler returned wrong response code: got %d, wanted %d", rr.Code, http.StatusOK)
 	}
 
 	req, _ = http.NewRequest("GET", "/make-reservation", nil)
-	ctx = getCtx(req)
+	ctx = getCtx(*req)
 	req = req.WithContext(ctx)
 	rr = httptest.NewRecorder()
 
-	handlers.ServeHTTP(rr, req)
+	handler.ServeHTTP(rr, req)
 	if rr.Code != http.StatusTemporaryRedirect {
-		t.Errorf("Reservation hundler returned wrong response code: got %d, wanted %d", rr.Code, http.StatusTemporaryRedirect)
+		t.Errorf("Reservation handler returned wrong response code: got %d, wanted %d", rr.Code, http.StatusTemporaryRedirect)
 	}
 }
 
