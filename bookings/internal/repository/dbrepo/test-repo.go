9ce7f41423bbffe329da4a9f349bb520d06f037f
eliberbell <liberbell@gmail.com>
2026-05-30 11:34:10 +0900

    test-repo1

--- bookings/internal/repository/dbrepo/test-repo.go
+++ bookings/internal/repository/dbrepo/test-repo.go
@@ -1,6 +1,7 @@
 package dbrepo
 
 import (
+	"errors"
 	"time"
 
 	"github.com/liber/bookings/internal/models"
@@ -30,5 +31,9 @@ func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]mode
 
 func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
 	var room models.Room
+	if id > 2 {
+		return room, errors.New("Some error")
+	}
+
 	return room, nil
 }
