861cc6fe24c7a1d3894935315c29120254c7450a
eliberbell <liberbell@gmail.com>
2026-05-31 11:04:46 +0900

    handlers7

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -124,23 +124,15 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err := r.ParseForm()
-	if err != nil {
-		m.App.Session.Put(r.Context(), "error", "can't parse start date")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Phone:     r.Form.Get("phone"),
+		Email:     r.Form.Get("email"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
-
-	reservation.FirstName = r.Form.Get("first_name")
-	reservation.LastName = r.Form.Get("last_name")
-	reservation.Phone = r.Form.Get("phone")
-	reservation.Email = r.Form.Get("email")
 
 	form := forms.New(r.PostForm)
 	form.Required("first_name", "last_name", "email")
