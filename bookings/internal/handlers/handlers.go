890a144694584bb207054e093eeaedb20a8f69d9
eliberbell <liberbell@gmail.com>
2026-06-08 08:46:02 +0900

    handlers2

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -395,5 +395,7 @@ func (m *Repository) BookRoom(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) ShowLogin(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "login.page.tmpl", &models.TemplateData{})
+	render.Template(w, r, "login.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
 }
