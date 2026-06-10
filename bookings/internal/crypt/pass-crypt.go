8926f7150b3a9edc4d492b6855a4e5abb08ba7f2
eliberbell <liberbell@gmail.com>
2026-06-11 08:46:41 +0900

    pas-crypt1

---
+++ bookings/internal/crypt/pass-crypt.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	password := "jamespass"
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
+	fmt.Println(hashedPassword)
+}
