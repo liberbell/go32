15ed8cb386d8255f1c8af8cfd8a4dcf880899111
eliberbell <liberbell@gmail.com>
2026-06-12 08:50:53 +0900

    routes1

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -39,5 +39,11 @@ func routes(app *config.AppConfig) http.Handler {
 	fileserver := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
 
+	mux.Route("/admin", func(mux chi.Router) {
+		mux.Use(Auth)
+
+		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+	})
+
 	return mux
 }
