0400a5fece26a4c5d9753250d9a949866110b7cf
eliberbell <liberbell@gmail.com>
2026-06-12 08:35:32 +0900

    handlers1

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -437,3 +437,9 @@ func (m *Repository) PostShowLogin(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "flash", "Login successfully")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.Destroy(r.Context())
+	_ = m.App.Session.RenewToken(r.Context())
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+}
