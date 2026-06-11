91a3e485a5d76bbe2425034b2cbe884de8dca38e
eliberbell <liberbell@gmail.com>
2026-06-12 08:47:29 +0900

    handlers2

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -443,3 +443,7 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 	_ = m.App.Session.RenewToken(r.Context())
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
+
+func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{})
+}
