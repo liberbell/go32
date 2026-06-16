b86d5fdbcd688e1820573058aec2739a19ccc8ff
eliberbell <liberbell@gmail.com>
2026-06-17 08:52:53 +0900

    handlers1

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -453,6 +453,12 @@ func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request
 }
 
 func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
+	reservations, err := m.DB.AllReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	data := make(map[string]interface{})
 	render.Template(w, r, "admin-all-reservations.page.tmpl", &models.TemplateData{})
 }
 
