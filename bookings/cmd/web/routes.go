09d4c0d9045d5981136ca04c02ed24e451e74500
eliberbell <liberbell@gmail.com>
2026-06-26 15:13:17 +0900

    routes1

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -45,7 +45,7 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
 		mux.Get("/reservations-new", handlers.Repo.AdminNewReservations)
 		mux.Get("/reservations-all", handlers.Repo.AdminAllReservations)
-		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalender)
+		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
 
 		mux.Get("/reservations/{src}/{id}", handlers.Repo.AdminShowReservation)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
