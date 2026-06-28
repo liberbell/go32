3e205ed5e07b254c3d1c29e9e925a7497c5738b1
eliberbell <liberbell@gmail.com>
2026-06-28 10:31:38 +0900

    handlers10

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -564,6 +564,9 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 	stringMap["last_month"] = lastMonth
 	stringMap["last_month_year"] = lastMonthYear
 
+	stringMap["this_month"] = now.Format("01")
+	stringMap["this_month_year"] = now.Format("2006")
+
 	render.Template(w, r, "admin-reservations-calender.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
