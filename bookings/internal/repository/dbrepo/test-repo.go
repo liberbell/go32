c187dcf855095edeee81b3c5a98c5803309e6da6
eliberbell <liberbell@gmail.com>
2026-06-30 08:37:09 +0900

    test-repo1

--- bookings/internal/repository/dbrepo/test-repo.go
+++ bookings/internal/repository/dbrepo/test-repo.go
@@ -88,3 +88,9 @@ func (m *testDBRepo) UpdateProcessedReservation(id, processed int) error {
 
 	return nil
 }
+
+func (m *testDBRepo) AllRooms() ([]models.Room, error) {
+	var rooms []models.Room
+
+	return rooms, nil
+}
