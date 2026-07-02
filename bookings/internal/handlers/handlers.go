a02ca0c6809f2055f257de2f735bbdb64d205da3
eliberbell <liberbell@gmail.com>
2026-07-03 08:45:18 +0900

    handlers2

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -603,8 +603,10 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 		for _, y ;= range restrictions {
 			if y.ReservationID > 0 {
 				for d := y.StartDate; d.After(y.EndDate) == false; d = d.AddDate(0, 0, 1) {
-					reservationMap[d.formatDate("2006-01-02")]
+					reservationMap[d.formatDate("2006-01-02")] = y.ReservationID
 				}
+			} else {
+				blockMap[y.Start]
 			}
 		}
 	}
