1824deebb3598908e8414fc98224a957b5568f34
eliberbell <liberbell@gmail.com>
2026-07-05 16:52:39 +0900

    handlers2

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -639,3 +639,7 @@ func (m *Repository) AdminDeleteReservation(w http.ResponseWriter, r *http.Reque
 	m.App.Session.Put(r.Context(), "flash", "Reservation deleted")
 	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
+
+func (m *Repository) AdminPostReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	log.Println("works2")
+}
