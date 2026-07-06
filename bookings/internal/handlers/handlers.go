31d214e7047d1fc63117c3850325479edd58543a
eliberbell <liberbell@gmail.com>
2026-07-06 09:02:50 +0900

    handlers5

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -651,4 +651,4 @@ func (m *Repository) AdminPostReservationsCalendar(w http.ResponseWriter, r *htt
 	month, _ := strconv.Atoi(r.Form.Get("m"))
 
 	m.App.Session.Put(r.Context(), "flash", "Changes saved")
-	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-calendar?y=%d&m=%d", y, m))
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-calendar?y=%d&m=%d", year, month), http.StatusSeeOther)
